b6135233b52e12118f4338c7c6fe909ee582ec14
Brian Lough <ecenuig@gmail.com>
2012-11-15 22:08:30 +0000

    Adding about and contact page

---
+++ about.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once('Common/Layouts.php');
+require_once('Common/Common.php');
+
+displayHeader();
+
+echo '
+			<table width="800" id="about" >
+				<tr width="800">About:<br />
+				<br />
+				CheckSmyths is a website that can check current stock level for a product in all stores on Toys.ie.<br />
+				<br />
+				It is particularly useful for bargain hunters has it can quickly tell you if there is any stock of an item in the country!<br />
+				<br />
+				CheckSmyths is not affiliated with Smyths in anyway.<br />
+				<br />
+				Please note for data purposes the website keeps a record of what item was searched for and what time the search was made,<br />
+				in future this may be used for a "most searched for" section. There is no other information that product number and a time stamp<br /> 
+				(time and date) recorded. Please contact me if you feel this is a problem.<br />
+				
+				</tr>';
+
+
+displayFooter();
+		
+		
+?>	
---
+++ contact.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once('Common/Layouts.php');
+require_once('Common/Common.php');
+
+displayHeader();
+
+echo '
+			<table width="800" id="contact" >
+				<tr>Contact: <br />
+				<br />
+				Email me at brian[a]consoledoc.com or get in touch with me on boards.ie (user: witnessmenow).
+				
+				</tr>';
+
+
+displayFooter();
+		
+		
+	
+?>
